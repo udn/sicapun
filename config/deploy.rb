eca02495b073852dd79ffdc80af2be6e039b88a7
Burhanudin Hakim <udnpico@gmail.com>
2016-02-05 08:57:02 +0700

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,23 +6,15 @@ set :repo_url, "git@github.com:udn/sicapun.git"
 set :rvm_type, :user
 set :rvm_ruby_version, '2.2.0@qontak-api'
 set :unicorn_example, "config/unicorn/unicorn.rb"
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :branch, :master
 set :use_sudo, false
 set :bundle_binstubs, nil
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :preload_app, true
-# SSHKit.config.command_map[:bundle] = "/home/deployer/.rvm/gems/ruby-2.2.0@global/bin/bundle"
 namespace :deploy do
   task :restart do
     invoke 'unicorn:legacy_restart'
   end
-  # desc 'run bundle install'
-  # task :install_bundle do
-  #   run "cd #{current_path} && bundle install  --without=test --no-update-sources"
-  # end
 end
 
 after 'deploy:publishing', 'deploy:restart'
-# before "deploy:restart", 'deploy:install_bundle'
