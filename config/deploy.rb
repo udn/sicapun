1c19ddc093589c67acff1337c8896ff489255733
Burhanudin Hakim <udnpico@gmail.com>
2016-02-05 08:41:19 +0700

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,9 +20,9 @@ namespace :deploy do
   end
   desc 'run bundle install'
   task :install_bundle do
-    run "cd #{current_path} && bundle install"
+    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
   end
 end
 
 after 'deploy:publishing', 'deploy:restart'
-after "deploy", 'deploy:install_bundle'
+before "deploy:restart", 'deploy:install_bundle'
