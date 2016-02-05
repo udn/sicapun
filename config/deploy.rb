b9c59f7c6f16464dc2d3c44ef3e6e4df3faa9a77
Burhanudin Hakim <udnpico@gmail.com>
2016-02-05 07:31:43 +0700

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,18 +22,9 @@ after 'deploy:publishing', 'deploy:restart'
 #   end
 # end
 
-set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :unicorn_conf, "#{deploy_to}/current/config/unicorn/unicorn.rb"
-set :unicorn_pid, "#{deploy_to}/current/tmp/pids/unicorn.pid"
 
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
-  end
-  task :start do
-    run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
-  end
-  task :stop do
-    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+    invoke 'unicorn:reload'
   end
 end
